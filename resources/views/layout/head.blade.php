64e7f630a66626eb52c3a6df4eb995f48bcb94dd
HanJinna <116608753+HanJinna@users.noreply.github.com>
2022-11-28 11:13:03 +0700

    Update head.blade.php

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -3,6 +3,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset ('Template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
