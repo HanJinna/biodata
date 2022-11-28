91a5e922fc175637f7da1dea195643020d3807d9
HanJinna <116608753+HanJinna@users.noreply.github.com>
2022-11-28 12:12:13 +0700

    Update head.blade.php

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -9,6 +9,7 @@
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
+<script src="https://kit.fontawesome.com/c16c6062fb.js" crossorigin="anonymous"></script>
 
     <!-- Custom fonts for this template-->
     <link href="asset https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
